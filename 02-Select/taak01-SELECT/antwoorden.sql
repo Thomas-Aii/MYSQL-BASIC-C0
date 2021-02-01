d97b69b0950fa267ae637fd5d60ec6f0597b4a03
Thomas Pos <thomas.pos@student.rocva.nl>
2021-02-01 11:25:38 +0100

    antwoorden ingevuld

--- 02-Select/taak01-SELECT/antwoorden.sql
+++ 02-Select/taak01-SELECT/antwoorden.sql
@@ -1,13 +1,13 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
 -- Opdracht 2 
-
+SELECT * FROM jaar2015
 -- Opdracht 3
-
+SELECT counrty, score from jaar2016
 -- Opdracht 4
-
+SELECT region from jaar2015
 -- Opdracht 5
-
+SELECT region, score from jaar2015
 -- Opdracht 6
-
+SELECT score, rank from jaar2016
 -- Opdracht 7
