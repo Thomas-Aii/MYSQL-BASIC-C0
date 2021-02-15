cdcf258e10f39f74eaba41a7687237ae0d4adf10
Thomas Pos <thomas.pos@student.rocva.nl>
2021-02-15 14:02:37 +0100

    opdracht gemaakt

--- opdracht/antwoorden.sql
+++ opdracht/antwoorden.sql
@@ -17,13 +17,14 @@ SELECT SUM(Global_Sales) AS totaal_verkochte_spellen_nintendo FROM videogamesale
 -- Opdracht 9
 SELECT name, year FROM videogamesales WHERE genre = "Racing" AND publisher ="nintendo" OR "Activision"
 -- Opdracht 10
-
+SELECT AVG(NA_Sales) AS gemiddelde_verkoop_noord_amerika, AVG(EU_Sales) AS gemiddelde_verkoop_europa, AVG(JP_Sales) AS gemiddelde_verkoop_japan FROM videogamesales
 -- Opdracht 11
-
+DELETE FROM videogamesales WHERE id = 80
 -- Opdracht 12
-
+DELETE FROM videogamesales WHERE year = 2012 OR publisher = "Ubisoft"
 -- Opdracht 13
-
+DELETE FROM videogamesales WHERE genre = "Adventure" AND publisher = "nintendo"
 -- Opdracht 14
-
+DELETE FROM videogamesales WHERE Global_Sales <= 1000 AND publisher = "nintendo"
 -- Opdracht 15
+DELETE FROM videogamesales WHERE year = 1997 AND NA_Sales >= 200.000
